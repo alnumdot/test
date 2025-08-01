7318a5628b0b50695f999b9eb40237c588d269e2
alnumdot <3by@tutanota.com>
2025-08-01 02:13:47 +0200

    Update main.cpp

--- r6_dll/main.cpp
+++ r6_dll/main.cpp
@@ -13,21 +13,28 @@ void allocate_console() {
 
 void main() {
     allocate_console();
-    printf("start");
+    
     g_module = (uintptr_t)GetModuleHandleA("RainbowSix.exe");
     std::thread cache_thread(cache.setup_cache);
     cache_thread.detach();
     
+    printf("start\nmod base: 0x%llx\n", g_module);
+
     while (true) {
-        //for (auto& actor : cache.actor_list) {
-        //    if (actor->is_actor_player()) {
-        //        auto pos = actor->get_actor_position();
-        //        printf("Position: X=%.2f, Y=%.2f, Z=%.2f\n", pos.x, pos.y, pos.z);
-        //    }
-        //}
+        if (!cache.camera) continue;
+
+        for (auto& actor : cache.actor_list) {
+
+            if (!actor) continue;
+
+            //if (actor->is_actor_player()) {
+                auto pos = actor->get_actor_position();
+                printf("Position: X=%.2f, Y=%.2f, Z=%.2f\n", pos.x, pos.y, pos.z);
+            //}
+        }
         Sleep(50);
     }
-    printf("end");
+    printf("end\n");
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
